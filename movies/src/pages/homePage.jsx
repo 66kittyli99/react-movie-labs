5d94960e03e3fd861991fbdd195f0038477b0686
Kitty Li <kittyli8113@gmail.com>
2025-03-25 20:57:11 +0000

    Added filtering of movie list - Movie lab 1 complete!

--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -5,6 +5,24 @@ const HomePage = (props) => {
   const [movies, setMovies] = useState([]);
   const favorites = movies.filter(m => m.favorite)
   localStorage.setItem('favorites', JSON.stringify(favorites))
+ 
+  const [nameFilter, setNameFilter] = useState("");
+  const [genreFilter, setGenreFilter] = useState("0");
+  const genreId = Number(genreFilter);
+
+  let displayedMovies = movies
+    .filter((m) => {
+      return m.title.toLowerCase().search(nameFilter.toLowerCase()) !== -1;
+    })
+    .filter((m) => {
+      return genreId > 0 ? m.genre_ids.includes(genreId) : true;
+    });
+
+  const handleChange = (type, value) => {
+    if (type === "name") setNameFilter(value);
+    else setGenreFilter(value);
+  };
+
 
   const addToFavorites = (movieId) => {
     const updatedMovies = movies.map((m) =>
