7e7e4e9cbb9ef1c2fe32aa45ca4908b8d9389acf
Kitty Li <kittyli8113@gmail.com>
2025-04-01 23:17:10 +0100

    Movie Details Page rendering.

--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -3,9 +3,22 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
+import MovieDetailsPage from './pages/movieDetailsPage'
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 
+const images = [
+  "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+  "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
+  "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
+  "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
+]
 const App = () => {
+  return (
+      <MovieDetailsPage movie={sample} images={images} />
+      );
+};
+
+//const App = () => {
   return (
     <BrowserRouter>
       <ul>
@@ -24,7 +37,7 @@ const App = () => {
       </Routes>
     </BrowserRouter>
   );
-};
+//};
 
 const rootElement = createRoot( document.getElementById("root") )
 rootElement.render(<App />);
