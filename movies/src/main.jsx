ccdd8ca9d29f03e574eba755ac3ecdbb320a8df9
Kitty Li <kittyli8113@gmail.com>
2025-04-02 01:06:48 +0100

    Change site header.

--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -1,44 +1,26 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
-import MovieDetailsPage from './pages/movieDetailsPage'
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
+import MovieReviewPage from "./pages/movieReviewPage";
+import SiteHeader from './components/siteHeader'
 
-const images = [
-  "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
-  "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
-  "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
-  "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
-]
 const App = () => {
   return (
-      <MovieDetailsPage movie={sample} images={images} />
-      );
-};
-
-//const App = () => {
-  return (
     <BrowserRouter>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/movies/favorites">Favorites</Link>
-        </li>
-      </ul>
-      <Routes>
-        <Route exact path="/movies/favorites" element={<FavoriteMoviesPage />} />
-        <Route path="/movies/:id" element={<MoviePage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={ <Navigate to="/" /> } />
-      </Routes>
-    </BrowserRouter>
+    <SiteHeader />
+    <Routes>
+      <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+      <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
+      <Route path="/movies/:id" element={<MoviePage />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="*" element={ <Navigate to="/" /> } />
+    </Routes>
+  </BrowserRouter>
+
   );
-//};
+};
 
 const rootElement = createRoot( document.getElementById("root") )
 rootElement.render(<App />);
-
